01b713762eed53fc8e18e2914bffa1872e759429
Ioseb Koplatadze <ioseb.koplatadze@singular.uk>
2019-10-03 21:34:21 +0400

    empty views

--- src/router.js
+++ src/router.js
@@ -1,26 +1,35 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: '/',
+      redirect: '/weathers',
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      path: '/weathers',
+      name: 'weathers-list',
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
-  ]
+        import(
+          /* webpackChunkName: "bundle.weathers" */ './views/weathers.vue'
+        ),
+    },
+    {
+      path: '/weathers/:city',
+      name: 'weather-detail',
+      component: () =>
+        import(
+          /* webpackChunkName: "bundle.weathers" */ './views/weather-detail.vue'
+        ),
+    },
+    {
+      path: '*',
+      redirect: '/weathers',
+    },
+  ],
 });
